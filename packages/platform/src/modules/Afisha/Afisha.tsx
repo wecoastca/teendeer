b06656c7b9654efa6bcf09728fc83dc4be81e0eb
Anton Shishov <foxconq@gmail.com>
2021-10-16 20:19:56 +0500

    Add afisha page

--- packages/platform/src/modules/Afisha/Afisha.tsx
+++ packages/platform/src/modules/Afisha/Afisha.tsx
@@ -1,7 +1,56 @@
+import { Card, Col, Row } from 'antd'
 import React, { FC } from 'react'
+import styles from './Afisha.module.less';
+import './Afisha.module.less';
+import img from 'assets/conf.jpeg';
+
+const { Meta } = Card;
 
 export const Afisha: FC = () => {
-    return(
-        <h1>fds</h1>
+    return (
+        <>
+            <Row>
+                <h1>Конференции</h1>
+            </Row>
+            <Row gutter={16}>
+                <Col span={12}>
+                    <Card className={styles.cardEvent} hoverable cover={<img alt="conf" src={img} />}>
+                        <Meta title={"Событие"} description="Описание события" />
+                    </Card>
+                </Col>
+                <Col span={12}>
+                    <Card className={styles.cardEvent} hoverable cover={<img alt="conf" src={img} />}>
+                        <Meta title={"Событие"} description="Описание события" />
+                    </Card>
+                </Col>
+            </Row>
+            <Row gutter={16}>
+                <Col span={12}>
+                    <Card className={styles.cardEvent} hoverable cover={<img alt="conf" src={img} />}>
+                        <Meta title={"Событие"} description="Описание события" />
+                    </Card>
+                </Col>
+                <Col span={12}>
+                    <Card className={styles.cardEvent} hoverable cover={<img alt="conf" src={img} />}>
+                        <Meta title={"Событие"} description="Описание события" />
+                    </Card>
+                </Col>
+            </Row>
+            <Row>
+                <h1>Конференции</h1>
+            </Row>
+            <Row gutter={16}>
+                <Col span={12}>
+                    <Card className={styles.cardEvent} hoverable cover={<img alt="conf" src={img} />}>
+                        <Meta title={"Событие"} description="Описание события" />
+                    </Card>
+                </Col>
+                <Col span={12}>
+                    <Card className={styles.cardEvent} hoverable cover={<img alt="conf" src={img} />}>
+                        <Meta title={"Событие"} description="Описание события" />
+                    </Card>
+                </Col>
+            </Row>
+        </>
     )
 }
