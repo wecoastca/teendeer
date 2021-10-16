eb73d83d7fd4d0e649b51fc50a3640963a97730f
Evgeny Bykovskikh <redzumi.tao@gmail.com>
2021-10-16 18:38:43 +0300

    Add routes

--- packages/dashboard/src/components/AppHeader/AppHeader.tsx
+++ packages/dashboard/src/components/AppHeader/AppHeader.tsx
@@ -2,17 +2,28 @@ import React from 'react';
 import { Layout, Menu } from 'antd';
 
 import styles from './styles.module.less';
+import { useHistory } from 'react-router-dom';
 
 const { Header } = Layout;
 
 const AppHeader = () => {
+  const history = useHistory();
+
+  const handleSelect = (info: { key: string }) => {
+    history.push(info?.key);
+  };
+
   return (
     <Header className={styles.header}>
       <div className={styles.logo} />
-      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
-        <Menu.Item key="1">Главная</Menu.Item>
-        <Menu.Item key="2">Пользователи</Menu.Item>
-        <Menu.Item key="3">Аналитика</Menu.Item>
+      <Menu
+        theme="dark"
+        mode="horizontal"
+        defaultSelectedKeys={['1']}
+        onSelect={handleSelect}>
+        <Menu.Item key="/">Главная</Menu.Item>
+        <Menu.Item key="/users">Пользователи</Menu.Item>
+        <Menu.Item key="/404">Аналитика</Menu.Item>
       </Menu>
     </Header>
   );
