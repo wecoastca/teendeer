ffb98ff37b91f1b9d56cf15d79cc01d2f10baa69
Evgeny Bykovskikh <redzumi.tao@gmail.com>
2021-10-16 16:28:50 +0300

    Fix navs

--- packages/platform/src/components/Layout/Navbar/Navbar.tsx
+++ packages/platform/src/components/Layout/Navbar/Navbar.tsx
@@ -1,32 +1,40 @@
-import { Button } from 'antd';
+import { Button, Divider } from 'antd';
+import cn from 'classnames';
 import { Footer } from 'antd/lib/layout/layout';
 import React, { FC } from 'react';
 import styles from './Navbar.module.less';
 import { ClearOutlined } from '@ant-design/icons';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 export const Navbar: FC = () => {
+  const TABS = [
+    { name: 'Афиша', pathname: '/afisha', icon: <ClearOutlined /> },
+    { name: 'Достижения', pathname: '/achieve', icon: <ClearOutlined /> },
+    {
+      name: 'Челенджи',
+      pathname: '/challenges',
+      icon: <ClearOutlined />,
+      target: true,
+    },
+    { name: 'Магазин', pathname: '/store', icon: <ClearOutlined /> },
+    { name: 'Профиль', pathname: '/profile', icon: <ClearOutlined /> },
+  ];
 
-    return (
-        <Footer className={styles.navbar}>
-            <div className={styles.buttonContainer}>
-                <Button icon={<ClearOutlined />} className={styles.navbarButton}>
-                    <Link to="/afisha">Афиша</Link>
-                </Button>
-                <Button icon={<ClearOutlined />} className={styles.navbarButton}>
-                    <Link to="/achieve">Достижения</Link>
-                </Button>
-                <Button icon={<ClearOutlined />} className={styles.navbarButton}>
-                    <Link to="/challenges">Челенджи</Link>
-                </Button>
-                <Button icon={<ClearOutlined />} className={styles.navbarButton}>
-                    <Link to="/store">Магазин</Link>
-                </Button>
-                <Button icon={<ClearOutlined />} className={styles.navbarButton}>
-                    <Link to="/profile">Профиль</Link>
-                </Button>
-
-            </div>
-        </Footer>
-    )
-}
+  return (
+    <Footer className={styles.navbar}>
+      <div className={styles.buttonContainer}>
+        {TABS.map((tab) => (
+          <NavLink
+            to={tab.pathname}
+            activeClassName={cn(styles.active)}
+            className={cn(styles.navbarButton, {
+              [styles.target]: tab.target,
+            })}>
+            {tab.icon}
+            {tab.name}
+          </NavLink>
+        ))}
+      </div>
+    </Footer>
+  );
+};
